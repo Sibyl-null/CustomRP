3eedb695e10dc6bd15c7f64847f6b6475c0de287
yangboyuan <yangboyuan@dailyinnovation.biz>
2023-10-17 15:05:43 +0800

    [feat]: 扩展AlphaClip的应用

--- Assets/CustomRP/Examples/PerObjectMaterialProperties.cs
+++ Assets/CustomRP/Examples/PerObjectMaterialProperties.cs
@@ -6,9 +6,11 @@ namespace CustomRP.Examples
     public class PerObjectMaterialProperties : MonoBehaviour
     {
         private static int _baseColorId = Shader.PropertyToID("_BaseColor");
+        private static int _cutoffId = Shader.PropertyToID("_Cutoff");
         private static MaterialPropertyBlock _block;
         
         [SerializeField] private Color _baseColor = Color.white;
+        [SerializeField, Range(0f, 1f)] private float _cutoff = 0.5f;
 
         private void OnValidate()
         {
@@ -16,6 +18,7 @@ namespace CustomRP.Examples
                 _block = new MaterialPropertyBlock();
             
             _block.SetColor(_baseColorId, _baseColor);
+            _block.SetFloat(_cutoffId, _cutoff);
             GetComponent<Renderer>().SetPropertyBlock(_block);
         }
 
