a5b77e980129975a923904b954e7defd183bd5ef
yangboyuan <yangboyuan@dailyinnovation.biz>
2023-10-18 15:23:34 +0800

    [feat]: 镜面反射 + 漫反射

--- Assets/CustomRP/ShaderLibrary/Surface.hlsl
+++ Assets/CustomRP/ShaderLibrary/Surface.hlsl
@@ -7,6 +7,7 @@ struct Surface
 {
     float3 normal;
     float3 color;
+    float3 viewDirection;
     float alpha;
     float metallic;
     float smoothness;
--- Assets/CustomRP/Shaders/LitPass.hlsl
+++ Assets/CustomRP/Shaders/LitPass.hlsl
@@ -3,7 +3,6 @@
 
 #include "../ShaderLibrary/Common.hlsl"
 #include "../ShaderLibrary/Surface.hlsl"
-#include "../ShaderLibrary/Light.hlsl"
 #include "../ShaderLibrary/Lighting.hlsl"
 
 TEXTURE2D(_BaseMap);
@@ -28,6 +27,7 @@ struct Attributes
 struct Varyings
 {
     float4 positionCS : SV_POSITION;
+    float3 positionWS : VAR_POSITION;
     float3 normalWS : VAR_NORMAL;
     float2 baseUV : VAR_BASE_UV;
     UNITY_VERTEX_INPUT_INSTANCE_ID
@@ -39,8 +39,8 @@ Varyings LitPassVertex(Attributes input)
     UNITY_SETUP_INSTANCE_ID(input);
     UNITY_TRANSFER_INSTANCE_ID(input, output);
     
-    float3 positionWS = TransformObjectToWorld(input.positionOS);
-    output.positionCS = TransformWorldToHClip(positionWS);
+    output.positionWS = TransformObjectToWorld(input.positionOS);
+    output.positionCS = TransformWorldToHClip(output.positionWS);
 
     output.normalWS = TransformObjectToWorldNormal(input.normalOS);
 
@@ -62,11 +62,13 @@ float4 LitPassFragment(Varyings input) : SV_TARGET
     Surface surface;
     surface.normal = normalize(input.normalWS);
     surface.color = base.rgb;
+    surface.viewDirection = normalize(_WorldSpaceCameraPos - input.positionWS);
     surface.alpha = base.a;
     surface.metallic = UNITY_ACCESS_INSTANCED_PROP(UnityPerMaterial, _Metallic);
     surface.smoothness = UNITY_ACCESS_INSTANCED_PROP(UnityPerMaterial, _Smoothness);
 
-    float3 color = GetLighting(surface);
+    BRDF brdf = GetBRDF(surface);
+    float3 color = GetLighting(surface, brdf);
     return float4(color, surface.alpha);
 }
 
